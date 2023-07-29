4ad30096c5f215c93271fd6fca82d0848fc77e6c
Yair Hengali <yairhengali@gmail.com>
2023-07-29 20:32:11 +0300

    trying things in Increase

--- SystemEngine/src/engine/action/Increase.java
+++ SystemEngine/src/engine/action/Increase.java
@@ -6,25 +6,39 @@ import engine.Property;
 public class Increase implements Action{
     Entity mainEntity;
     Property<Number> property;
-    int amountToIncrease;
+    Number amountToIncrease;
 
-    public Increase(Entity mainEntity, String propertyName, int amountToIncrease){
+//    public Increase(Entity mainEntity, String propertyName, int amountToIncrease){
+//        this.mainEntity = mainEntity;
+//        if (this.mainEntity.getPropertyByName(propertyName).getValue() instanceof Number)//IN real TIME WILL BE OBJECT, PROBLEM
+//        {
+//            this.property = (Property<Number>) this.mainEntity.getPropertyByName(propertyName);
+//        }
+//        this.amountToIncrease = amountToIncrease; //TODO: NEED TO UNDERSTAND IN CASE OF VALUE THAT DEPENDS ENVIRONMENT
+//    }
+    public Increase(Entity mainEntity, Property<Number> property, Number amountToIncrease){
         this.mainEntity = mainEntity;
-        if (this.mainEntity.getPropertyByName(propertyName).getValue() instanceof Number)
-        {
-            this.property = (Property<Number>) this.mainEntity.getPropertyByName(propertyName);
-        }
+        this.property = property;
         this.amountToIncrease = amountToIncrease; //TODO: NEED TO UNDERSTAND IN CASE OF VALUE THAT DEPENDS ENVIRONMENT
     }
 
     @Override
     public void Run() {
-        mainEntity.getPropertyByName(propertyName).setValue(mainEntity.getPropertyByName(propertyName).getValue() + amountToIncrease);
+//        mainEntity.getPropertyByName(propertyName).setValue(mainEntity.getPropertyByName(propertyName).getValue() + amountToIncrease);
+        Number propertyValue = property.getValue();
+        if (propertyValue instanceof Integer) {
+            int intValue = propertyValue.intValue();
+            property.setValue(intValue + amountToIncrease.intValue());
+        } else if (propertyValue instanceof Float) {
+            float floatValue = propertyValue.floatValue();
+            property.setValue(floatValue + amountToIncrease.floatValue());
+        }
+        else{
+            //NEED TO DO SOMETHING?
+            }
 
             //IF ENTITY.NAME == entityName
                 //FOREACH PROPERTY : GET PROPERTIES()
                     //IF PROPERTY.NAME == propertyName
-
-
     }
 }
