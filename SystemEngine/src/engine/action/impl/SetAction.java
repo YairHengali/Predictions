8bd6ae0af7a0c8f45ad211c19c24fdb105fb29df
Yair Hengali <yairhengali@gmail.com>
2023-08-11 01:02:27 +0300

    implemented run in SetAction

--- SystemEngine/src/engine/action/impl/SetAction.java
+++ SystemEngine/src/engine/action/impl/SetAction.java
@@ -1,8 +1,14 @@
 package engine.action.impl;
 
+import engine.YairExpression;
 import engine.action.api.AbstractAction;
 import engine.action.api.ActionType;
 import engine.context.Context;
+import engine.property.api.PropertyInstance;
+import engine.property.impl.BooleanProperty;
+import engine.property.impl.DecimalProperty;
+import engine.property.impl.FloatProperty;
+import engine.property.impl.StringProperty;
 
 public class SetAction extends AbstractAction {
     String propertyName;
@@ -15,6 +21,29 @@ public class SetAction extends AbstractAction {
 
     @Override
     public void Run(Context context) throws Exception {//TODO: EXCEPTION IF property from unmatching type
-        //TODO WITH EXPRESSIONS
+        YairExpression valueAsExpression = new YairExpression(valueExpression, context.getActiveEnvironmentVariables(), context.getPrimaryEntityInstance());
+        String valueFromExpression = valueAsExpression.praseExpressionToValueString();
+
+        PropertyInstance entityPropertyInstance = context.getPrimaryEntityInstance().getPropertyByName(propertyName);
+
+        try {
+            if (entityPropertyInstance instanceof DecimalProperty) {
+                ((DecimalProperty) entityPropertyInstance).setValue(Integer.parseInt(valueFromExpression));
+            }
+            else if (entityPropertyInstance instanceof FloatProperty) {
+                ((FloatProperty) entityPropertyInstance).setValue(Float.parseFloat(valueFromExpression));
+            }
+            else if (entityPropertyInstance instanceof BooleanProperty) {
+                ((BooleanProperty) entityPropertyInstance).setValue(Boolean.parseBoolean(valueFromExpression));
+            }
+            else if (entityPropertyInstance instanceof StringProperty) {
+                ((StringProperty) entityPropertyInstance).setValue(valueFromExpression);
+            }
+        } catch (NumberFormatException e) {//IF INTEGER / FLOAT PARSING UNSUCCESSFUL
+            //TODO: HANDLE THE PARSING EXCEPTION
+        } catch (IllegalArgumentException e) { //IF BOOLEAN PARSING UNSUCCESSFUL
+            //TODO: HANDLE THE PARSING EXCEPTION
+        }
+
     }
 }
