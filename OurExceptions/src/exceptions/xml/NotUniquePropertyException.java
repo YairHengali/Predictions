4d37fbf5a70618531856200c04d49077345f7ed3
Yair Hengali <yairhengali@gmail.com>
2023-08-16 21:49:29 +0300

    edded XMLs checking problems 3 and 5 in the list

--- OurExceptions/src/exceptions/xml/NotUniquePropertyException.java
+++ OurExceptions/src/exceptions/xml/NotUniquePropertyException.java
@@ -6,6 +6,6 @@ public class NotUniquePropertyException extends RuntimeException{
 
     public NotUniquePropertyException(String propertyName, String entityName)
     {
-        super("Invalid xml file! A property named " + propertyName + "is already exists in the entity: " + entityName + ". each property in an entity must have a unique name!");
+        super("Invalid xml file! A property named " + propertyName + " is already exists in the entity: " + entityName + ". each property in an entity must have a unique name!");
     }
 }
