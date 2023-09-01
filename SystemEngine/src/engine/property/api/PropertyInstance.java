f004aacd84f0d946feb6256d4a98abb58f8704c9
ofirNakdai <93441082+ofirNakdai@users.noreply.github.com>
2023-09-01 16:33:15 +0300

    setting time properties time stamp

--- SystemEngine/src/engine/property/api/PropertyInstance.java
+++ SystemEngine/src/engine/property/api/PropertyInstance.java
@@ -3,11 +3,16 @@ package engine.property.api;
 import engine.property.PropertyType;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public abstract class PropertyInstance implements Serializable {
     protected String value;
     private final String name;
     private final PropertyType type;
+
+    private Map<Integer, String> tick2value = new HashMap<>();
     private Integer lastTickModified = 0;
 
 
@@ -29,11 +34,12 @@ public abstract class PropertyInstance implements Serializable {
     }
 
     public int getLastTickModified() {
-        return lastTickModified;
+        Optional<Integer> lastModifiedTick = tick2value.keySet().stream().max(Integer::compare);
+        return lastModifiedTick.get();
     }
 
-    public void setLastTickModified(Integer lastTickModified) {
-        if(lastTickModified != null){
-            this.lastTickModified = lastTickModified;
+    public void setLastTickModified(Integer currTick) {
+        if(currTick != null){
+            this.tick2value.put(currTick, this.value);
         }
     }}
