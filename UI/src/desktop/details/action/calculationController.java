bba6533d33128e34b48335c14ac92c1e88cb98f7
Yair Hengali <yairhengali@gmail.com>
2023-09-03 01:38:09 +0300

    template for calculation action controller

---
+++ UI/src/desktop/details/action/calculationController.java
@@ -0,0 +1,21 @@
+package desktop.details.action;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+    public class calculationController {
+
+        @FXML private Label calcTypeLabel;
+
+        @FXML private Label mainEntLabel;
+
+        @FXML private Label resPropLabel;
+
+        @FXML private Label arg1Label;
+
+        @FXML private Label arg2Label;
+
+        public calculationController(){ //TODO, where do i get the details from? JAXB? CREATE ANOTHER DTOs ??
+
+    }
+}
